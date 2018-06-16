566a5b20d148f27688e5029867a7a571503a0df3
Bartek Łukawski <bwmn.peter@gmail.com>
2018-06-16 15:09:21 +0200

    Add missing DEPENDS option to ycm_ep_helper()
    Follows up c709e0a.

--- cmake/ycm-modules/BuildASROB_YARP_DEVICES.cmake
+++ cmake/ycm-modules/BuildASROB_YARP_DEVICES.cmake
@@ -6,4 +6,5 @@ find_or_build_package(COLOR_DEBUG QUIET)
 ycm_ep_helper(ASROB_YARP_DEVICES TYPE GIT
               STYLE GITHUB
               REPOSITORY asrob-uc3m/yarp-devices.git
-              TAG master)
+              TAG master
+              DEPENDS COLOR_DEBUG)
--- cmake/ycm-modules/BuildROBOTICSLAB_OPENRAVE_YARP_PLUGINS.cmake
+++ cmake/ycm-modules/BuildROBOTICSLAB_OPENRAVE_YARP_PLUGINS.cmake
@@ -6,4 +6,5 @@ find_or_build_package(COLOR_DEBUG QUIET)
 ycm_ep_helper(ROBOTICSLAB_OPENRAVE_YARP_PLUGINS TYPE GIT
               STYLE GITHUB
               REPOSITORY roboticslab-uc3m/openrave-yarp-plugins.git
-              TAG master)
+              TAG master
+              DEPENDS COLOR_DEBUG)
