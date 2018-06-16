d6b395ca75c8c4b7f18ae1cd109beb92a48f5373
Bartek Łukawski <bwmn.peter@gmail.com>
2018-06-16 20:20:34 +0200

    Pull higher-level dependencies via YCM

--- cmake/ycm-modules/BuildRD_OPENRAVE_MODELS.cmake
+++ cmake/ycm-modules/BuildRD_OPENRAVE_MODELS.cmake
@@ -1,6 +1,10 @@
 include(YCMEPHelper)
+include(FindOrBuildPackage)
+
+find_or_build_package(YARP QUIET)
 
 ycm_ep_helper(RD_OPENRAVE_MODELS TYPE GIT
               STYLE GITHUB
               REPOSITORY asrob-uc3m/robotDevastation-openrave-models.git
-              TAG master)
+              TAG master
+              DEPENDS YARP)
