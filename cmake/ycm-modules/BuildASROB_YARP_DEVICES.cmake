e428f052b9a427f96dca3e7664a60ffdf8cd2770
Bartek Łukawski <bwmn.peter@gmail.com>
2021-05-10 11:05:47 +0200

    Review CMake code and interdependencies

--- cmake/ycm-modules/BuildASROB_YARP_DEVICES.cmake
+++ cmake/ycm-modules/BuildASROB_YARP_DEVICES.cmake
@@ -2,10 +2,9 @@ include(YCMEPHelper)
 include(FindOrBuildPackage)
 
 find_or_build_package(YARP QUIET)
-find_or_build_package(COLOR_DEBUG QUIET)
 
 ycm_ep_helper(ASROB_YARP_DEVICES TYPE GIT
               STYLE GITHUB
               REPOSITORY asrob-uc3m/yarp-devices.git
               TAG master
-              DEPENDS "YARP;COLOR_DEBUG")
+              DEPENDS YARP)
